60a2c3b087e7d2d352b881785c37969f6dad56f9
neelamgharti <78545367+neelamgharti@users.noreply.github.com>
2021-03-18 20:49:02 +0530

    added comments

--- dao.java
+++ dao.java
@@ -11,11 +11,15 @@ import com.springboot.model.Student;
 import rx.Observable;
 
 public interface dao {;
-
+	//To fetch all the documents from the student Container
 	Observable<List<Student>> findAll();
+	//To fetch document by id from the student container
 	Observable<Student> findById(String id,String course);
+	// To save a document in the student container
 	Observable<Student> save(Student s);
+	// To update an existing document in the student container
 	Observable<Student> update(String id, Student student);
+	// To delete the document by id from the student container
 	void deleteById(String id,String course);
 	
 
